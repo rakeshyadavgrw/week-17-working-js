ff940c03bab7dc011b821141d59f0f33414020a4
rakeshyadavgrw <137645492+rakeshyadavgrw@users.noreply.github.com>
2024-01-13 17:03:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// *****************Question-1*****************
+
+// function addNumbers(num1,num2){
+//     return num1+num2
+// }
+// console.log(addNumbers(5,10));
+
+// *****************Question-2*****************
+
+// const multiplynumber = function (num1,num2){
+//     return num1*num2;
+// }
+// console.log(multiplynumber(3,4))
+
+ // *****************Question-3*****************
+
+// function addnumbers(num1,num2){
+// var sum = num1+num2;
+// return sum;
+// }
+
+// console.log(addnumbers(5,10))
+
+ // *****************Question-4*****************
+// {
+// var x = "pw";
+// let y = "skills";
+// const z = "!";
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+// }
+
+ // *****************Question-5*****************
+{
+    let x = "hello";
+    console.log(x);
+}
+
